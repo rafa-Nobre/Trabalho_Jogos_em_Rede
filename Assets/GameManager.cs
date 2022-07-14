6d10f05695ff8cc902e008d73d15645610c6cd81
donysRafa <50738060+donysRafa@users.noreply.github.com>
2022-07-14 16:51:42 -0300

    Chat/log funcional!

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -27,11 +27,17 @@ public class GameManager : MonoBehaviour
 
     public void SendMessageToChat(string text) {
         if(messageList.Count >= maxMessages) {
+            Destroy(messageList[0].textObject.gameObject);
             messageList.Remove(messageList[0]);
         }
 
         Message newMessage = new Message();
         newMessage.text = text;
+
+        GameObject newText = Instantiate(textObject, chatPanel.transform);
+
+        newMessage.textObject = newText.GetComponent<Text>();
+        newMessage.textObject.text = newMessage.text;
 
         messageList.Add(newMessage);
     }
@@ -40,4 +46,5 @@ public class GameManager : MonoBehaviour
 [System.Serializable]
 public class Message {
     public string text;
+    public Text textObject;
 }
