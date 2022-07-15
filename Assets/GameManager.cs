c40c14f9dd09f9f8197e439d8edd2bd859d9314c
donysRafa <50738060+donysRafa@users.noreply.github.com>
2022-07-15 00:01:19 -0300

    Chat finalizado!

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,7 +6,12 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public int maxMessages = 25;
+
+    public string userField;
     public GameObject chatPanel, textObject;
+    public InputField chatBox;
+
+    public Color playerMessage, info;
     
     [SerializeField]
     List<Message> messageList = new List<Message>();
@@ -15,17 +20,27 @@ public class GameManager : MonoBehaviour
     {
         
     }
-
     
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space)) {
-            SendMessageToChat("Space Pressed!");
-            Debug.Log("Space");
+    void Update() {
+        if(chatBox.text != "") {
+            if(Input.GetKeyDown(KeyCode.Return)) {
+                SendMessageToChat(userField + ": " + chatBox.text, Message.MessageType.playerMessage);
+                chatBox.text = "";
+            }
+        }else {
+            if(!chatBox.isFocused && Input.GetKeyDown(KeyCode.Return)) {
+                chatBox.ActivateInputField();
+            }
+        }
+        if(!chatBox.isFocused) {
+            if(Input.GetKeyDown(KeyCode.Space)) {
+                SendMessageToChat("Space Pressed!", Message.MessageType.info);
+                Debug.Log("Space");
+            }
         }
     }
 
-    public void SendMessageToChat(string text) {
+    public void SendMessageToChat(string text, Message.MessageType messageType) {
         if(messageList.Count >= maxMessages) {
             Destroy(messageList[0].textObject.gameObject);
             messageList.Remove(messageList[0]);
@@ -39,7 +54,21 @@ public class GameManager : MonoBehaviour
         newMessage.textObject = newText.GetComponent<Text>();
         newMessage.textObject.text = newMessage.text;
 
+        newMessage.textObject.color = MessageTypeColor(messageType);
+
         messageList.Add(newMessage);
+    }
+
+    Color MessageTypeColor(Message.MessageType messageType) {
+        Color currentColor = info;
+
+        switch(messageType) {
+            case Message.MessageType.playerMessage:
+                currentColor = playerMessage;
+                break;
+        }
+
+        return currentColor;
     }
 }
 
@@ -47,4 +76,9 @@ public class GameManager : MonoBehaviour
 public class Message {
     public string text;
     public Text textObject;
+    public MessageType messageType;
+    public enum MessageType {
+        playerMessage,
+        info
+    }
 }
