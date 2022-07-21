144a6cf5c231d05ce0ac33b2d1741017ee10825d
Sérgio Pires Barra <spbarra2022@gmail.com>
2022-07-21 15:17:00 -0300

    passando ws pro servidor

--- Assets/Scripts/WS_Client.cs
+++ Assets/Scripts/WS_Client.cs
@@ -31,8 +31,8 @@ public class WS_Client : MonoBehaviour
         }
         DontDestroyOnLoad(this.gameObject);
 
-        //ws = new WebSocket("ws://aeugame2022.herokuapp.com");
-        ws = new WebSocket("ws://localhost:8080"); //testar localmente
+        ws = new WebSocket("ws://aeugame2022.herokuapp.com");
+        //ws = new WebSocket("ws://localhost:8080"); //testar localmente
         ws.OnMessage += (sender, e) =>
         {
             //Debug.Log("Mensagem recebida de " + ((WebSocket)sender).Url + ", Dado: " + e.Data);
